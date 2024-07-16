edbb2e3bf95951356485b52a609a76bcd6436b88
Ziadhisham288 <ziadabomadina288@gmail.com>
2024-07-16 21:17:53 +0300

    fix

--- src/components/CustomerDetails.jsx
+++ src/components/CustomerDetails.jsx
@@ -1,5 +1,4 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ClipLoader from "react-spinners/ClipLoader";
 import {
@@ -14,29 +13,22 @@ import {
 } from "recharts";
 
 const CustomerDetails = () => {
-  const [customer, setCustomer] = useState(null); 
+  const [customer, setCustomer] = useState(null);
   const [transactions, setTransactions] = useState([]);
   const params = useParams();
 
-  const getCustomerData = async () => {
-    try {
-      const { data } = await axios.get(
-        `https://server-iota-murex.vercel.app/api/customers/${params.id}`, {
-          headers: {
-            'Cache-Control': 'no-cache'
-          }
-        }
-      );
-      setCustomer(data?.customer[0]);
-      setTransactions(data?.transactions);
-    } catch (error) {
-      console.error("Error fetching customer data:", error);
-    }
-  };
-
   useEffect(() => {
-    getCustomerData();
-  }, []);
+    const findCustomer = Data.customers.find(
+      (c) => c.id === parseInt(params.id)
+    );
+    if (findCustomer) {
+      setCustomer(findCustomer);
+      const customerTransactions = Data.transactions.filter(
+        (transaction) => transaction.customer_id === findCustomer.id
+      );
+      setTransactions(customerTransactions);
+    }
+  }, [params]);
 
   if (!customer) {
     return (
@@ -56,15 +48,14 @@ const CustomerDetails = () => {
       </h6>
       <div className="chartSection bg-white w-full shadow-xl rounded-[10px] py-4 px-2">
         <div className="header mb-5 border-b">
-          <h3 className="text-[18px] font-semibold text-gray-800">Account overview</h3>
+          <h3 className="text-[18px] font-semibold text-gray-800">
+            Account overview
+          </h3>
           <h6 className="text-gray-400 mb-2">Your account transactions.</h6>
         </div>
 
         <ResponsiveContainer width="90%" height={300}>
-          <BarChart
-            data={transactions}
-          
-          >
+          <BarChart data={transactions}>
             <XAxis dataKey="date" />
             <YAxis />
             <Tooltip />
